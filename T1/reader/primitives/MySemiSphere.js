e60d224c5ba2f70f0fafc5d4325d5b4a793c3ca5
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-09-26 19:18:42 +0100

    Semisphere and Sphere done
    Need to check textures and normals

--- T1/reader/primitives/MySemiSphere.js
+++ T1/reader/primitives/MySemiSphere.js
@@ -4,12 +4,12 @@
  */
 class MySemiSphere extends CGFobject
 {
-	constructor(scene, slices, radius, stacks)
+	constructor(scene, radius, slices, stacks)
 	{
 		super(scene);
 
 		this.slices = slices;
-        this.stacks = stacks;
+        this.stacks = stacks; //for each semisphere
         this.radius = radius;
 
 		this.initBuffers();
@@ -22,41 +22,48 @@ class MySemiSphere extends CGFobject
 		this.normals = [];
 		this.texCoords = [];
 
-		var alpha_angle = Math.PI/(2*this.stacks);
-		var omega_angle = 2*Math.PI/this.slices;
+		let theta_angle = 0.5*Math.PI/this.stacks;
+		let omega_angle = 2*Math.PI/this.slices;
 
-		for(var i = 0; i <= this.slices; i++) {
-
-			for(var j = 0; j <= this.stacks; j++) {
+		for(let i = 0; i <= this.slices; i++) {
+			for(let j = 0; j <= this.stacks; j++) {
                 
-                var x = this.radius*Math.cos(alpha_angle*j)*this.radius*Math.cos(omega_angle*i);
-                var y = this.radius*Math.cos(alpha_angle*j)*this.radius*Math.sin(omega_angle*i);
-                var z =  this.radius*Math.sin(alpha_angle*j);
-				this.vertices.push(x, y, z);
-                
-                if(i != this.slices && j != this.stacks) {
-                    var a = (i+1)*(this.stacks+1) + j;
-                    var b = i*(this.stacks+1) + j+1;
-                    var c = i*(this.stacks+1) + j;
-                    var d = i*(this.stacks+1) + j+1;
-                    var e = (i+1)*(this.stacks+1) + j;
-                    var f = (i+1)*(this.stacks+1) + j+1;
-                
-                    this.indices.push(a, b, c);
-					this.indices.push(d, e, f);
-					this.indices.push(c, b, a);
-					this.indices.push(f, e, d);
-                }
-
-				this.normals.push(x, y, z);
-
-				this.texCoords.push( (x+1) / 2, 1 - (y+1) / 2 );
-
+                let nx = this.radius*Math.cos(theta_angle*j)*Math.cos(omega_angle*i);
+                let ny = this.radius*Math.cos(theta_angle*j)*Math.sin(omega_angle*i);
+                let nz =  this.radius*Math.sin(theta_angle*j);
+				
+				this.vertices.push(this.radius*nx, this.radius*ny, this.radius*nz);
+				if(this.radius*nx==0) {
+					console.log('Hey');
+				}
+				this.normals.push(nx, ny, nz);
+				this.texCoords.push( 0.5*(this.radius*nx+1), 1 - 0.5*(this.radius*ny+1));
 			}
+		}
 
-        }
+		for(let i = 0; i < this.slices; i++) {
+			for(let j = 0; j < this.stacks; j++) {
+                
+                let a = (i+1)*(this.stacks+1) + j;
+				let b = i*(this.stacks+1) + j+1;
+				let c = i*(this.stacks+1) + j;
+
+				let d = i*(this.stacks+1) + j+1;
+				let e = (i+1)*(this.stacks+1) + j;
+				let f = (i+1)*(this.stacks+1) + j+1;
+
+				this.indices.push(a, b, c);
+				this.indices.push(d, e, f);
+				//Both sides visible
+				this.indices.push(c, b, a);
+				this.indices.push(f, e, d);
+			}
+		}
+	
         
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
 	};
 };
+
+
