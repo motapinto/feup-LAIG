bfd7408e83b40336adacf398d261cbecc2f31f18
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-10-01 12:15:22 +0100

    torus done ; sphere updated

--- T1/reader/primitives/MySphere.js
+++ T1/reader/primitives/MySphere.js
@@ -9,25 +9,58 @@ class MySphere extends CGFobject
 		super(scene);
 
 		this.slices = slices;
-        this.stacks = stacks;
+        this.stacks = stacks; //for each semisphere
         this.radius = radius;
-
-        this.top_half = new MySemiSphere(scene, radius, slices, stacks);
-        this.bottom_half = new MySemiSphere(scene, radius, slices, stacks);
 
 		this.initBuffers();
 	};
 
-	display() {
-        this.scene.pushMatrix();
-            this.top_half.display();
-        this.scene.popMatrix();
+	initBuffers()
+	{
+		this.vertices = [];
+		this.indices = [];
+		this.normals = [];
+		this.texCoords = [];
 
-        this.scene.pushMatrix();
-            this.scene.rotate(Math.PI, 1, 0, 0);
-            this.bottom_half.display();
-        this.scene.popMatrix();
-    }
+		let theta_angle = 2*Math.PI/this.stacks;
+		let omega_angle = 2*Math.PI/this.slices;
+
+		for(let i = 0; i <= this.slices; i++) {
+			for(let j = 0; j <= this.stacks; j++) {
+                
+                let nx = this.radius*Math.cos(theta_angle*j)*Math.cos(omega_angle*i);
+                let ny = this.radius*Math.cos(theta_angle*j)*Math.sin(omega_angle*i);
+                let nz =  this.radius*Math.sin(theta_angle*j);
+				
+				this.vertices.push(this.radius*nx, this.radius*ny, this.radius*nz);
+				this.normals.push(nx, ny, nz);
+				this.texCoords.push( 0.5*(this.radius*nx+1), 1 - 0.5*(this.radius*ny+1));
+			}
+		}
+
+		for(let i = 0; i < this.slices; i++) {
+			for(let j = 0; j < this.stacks; j++) {
+                
+                let a = (i+1)*(this.stacks+1) + j;
+				let b = i*(this.stacks+1) + j+1;
+				let c = i*(this.stacks+1) + j;
+
+				let d = i*(this.stacks+1) + j+1;
+				let e = (i+1)*(this.stacks+1) + j;
+				let f = (i+1)*(this.stacks+1) + j+1;
+
+				this.indices.push(a, b, c);
+				this.indices.push(d, e, f);
+				//Both sides visible
+				this.indices.push(c, b, a);
+				this.indices.push(f, e, d);
+			}
+		}
+	
+        
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	};
 };
 
 
