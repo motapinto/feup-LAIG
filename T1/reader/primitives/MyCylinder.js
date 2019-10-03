2f37b4201a3f8585dc3e2fedeaefbf4870355081
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-10-03 17:18:04 +0100

    before tp2 commit

--- T1/reader/primitives/MyCylinder.js
+++ T1/reader/primitives/MyCylinder.js
@@ -4,79 +4,83 @@
 */
 
 class MyCylinder extends CGFobject {
-  constructor(scene, base, top, height, slices, stacks) {
-      super(scene);
-      this.scene = scene;
+    constructor(scene, base, top, height, slices, stacks) {
+        super(scene);
+        this.scene = scene;
 
-      this.base = base;
-      this.top = top;
-      this.height = height;
-      this.slices = slices;
-      this.stacks = stacks;
+        this.slices = slices;
+        this.stacks = stacks;
+        this.height = height;
+        this.top = top;
+        this.base = base;
 
-      this.initBuffers();
-  }
+        this.initBuffers();
+    }
 
-  initBuffers() {
-      this.vertices = [];
-      this.indices = [];
-      this.normals = [];
-      this.texCoords = [];
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
 
-      var ang = 0;
-      var alphaAng = 2 * Math.PI / this.slices;
+        var ang = 0;
+        var alphaAng = 2*Math.PI/this.slices;
 
-      var delta_z = this.height / this.stacks;
-      var delta_radius = (this.top - this.base) / this.stacks;
-      
-      for(var i = 0; i < this.slices; i++) {
+        var delta_z = this.height / this.stacks;
+        var delta_radius = (this.top - this.base) / this.stacks;
         
-        var indices_increment = 4*i*this.stacks;
-        var x1_bottom = this.base*Math.cos(ang);
-        var y1_bottom = this.base*Math.sin(ang); 
-        var x2_bottom = this.base*Math.cos(ang+alphaAng);
-        var y2_bottom = this.base*Math.sin(ang+alphaAng);
-        
-        for(var j = 0; j < this.stacks; j++){
+        for(var i = 0; i < this.slices; i++) {
           
-          var radius =  this.base + (j + 1) * delta_radius;
+          var indices_increment = 4*i*this.stacks;
+          var x1_bottom=this.base*Math.cos(ang);
+          var y1_bottom=this.base*Math.sin(ang); 
+          var x2_bottom=this.base*Math.cos(ang+alphaAng);
+          var y2_bottom=this.base*Math.sin(ang+alphaAng);
           
-          var y1_top = radius*Math.sin(ang); 
-          var x2_top = radius*Math.cos(ang+alphaAng);
-          var y2_top = radius*Math.sin(ang+alphaAng);
-          var x1_top = radius*Math.cos(ang);
+          for(var j = 0; j < this.stacks; j++){
+            
+            var radius =  this.base + (j + 1) * delta_radius;
+            
+            var y1_top=radius*Math.sin(ang); 
+            var x2_top=radius*Math.cos(ang+alphaAng);
+            var y2_top=radius*Math.sin(ang+alphaAng);
+            var x1_top=radius*Math.cos(ang);
+            
+            this.vertices.push(x1_bottom, y1_bottom, delta_z*j);
+            this.vertices.push(x2_bottom, y2_bottom, delta_z*j);
+            this.vertices.push(x1_top, y1_top, delta_z*(j+1));
+            this.vertices.push(x2_top, y2_top, delta_z*(j+1));
           
-          this.vertices.push(x1_bottom, y1_bottom, delta_z*j);
-          this.vertices.push(x2_bottom, y2_bottom, delta_z*j);
-          this.vertices.push(x1_top, y1_top, delta_z*(j+1));
-          this.vertices.push(x2_top, y2_top, delta_z*(j+1));
-        
-          //visible from both sides
-          this.indices.push(indices_increment+4*j+1, indices_increment+4*j+3, indices_increment+4*j+2);
-          this.indices.push(indices_increment+4*j, indices_increment+4*j+1, indices_increment+4*j+2);  
-          this.indices.push(indices_increment+4*j+2, indices_increment+4*j+3, indices_increment+4*j+1);
-          this.indices.push(indices_increment+4*j+2, indices_increment+4*j+1, indices_increment+4*j);
-          
-          this.normals.push(x1_bottom, y1_bottom, 0);
-          this.normals.push(x2_bottom, y2_bottom, 0);  
-          this.normals.push(x1_top, y1_top, 0);     
-          this.normals.push(x2_top, y2_top, 0);
+            //visible from both sides
+            this.indices.push(indices_increment+4*j+1, indices_increment+4*j+3, indices_increment+4*j+2);
+            this.indices.push(indices_increment+4*j, indices_increment+4*j+1, indices_increment+4*j+2);  
+            this.indices.push(indices_increment+4*j+2, indices_increment+4*j+3, indices_increment+4*j+1);
+            this.indices.push(indices_increment+4*j+2, indices_increment+4*j+1, indices_increment+4*j);
+            
+            this.normals.push(x1_bottom, y1_bottom, 0);
+            this.normals.push(x2_bottom, y2_bottom, 0);  
+            this.normals.push(x1_top, y1_top, 0);     
+            this.normals.push(x2_top, y2_top, 0);
 
-          this.texCoords.push(0+i*(1.0/this.slices),                            1 - j*(1.0/this.stacks));
-          this.texCoords.push((1.0/this.slices) + i*(1.0/this.slices),          1 - j*(1.0/this.stacks));
-          this.texCoords.push(0+i*(1.0/this.slices),                            1 - ((1.0/this.stacks) + j*(1.0/this.stacks)));
-          this.texCoords.push((1.0/this.slices) + i*(1.0/this.slices),          1 - ((1.0/this.stacks) + j*(1.0/this.stacks)));
+            var y1 = 1 - j*(1.0/this.stacks)
+            var y2 = 1 - ((1.0/this.stacks) + j*(1.0/this.stacks))
 
-          x1_bottom = x1_top;
-          x2_bottom = x2_top;
-          y1_bottom = y1_top;
-          y2_bottom = y2_top;
+  
+            this.texCoords.push(0+i*(1.0/this.slices),                            1 - j*(1.0/this.stacks));
+            this.texCoords.push((1.0/this.slices) + i*(1.0/this.slices),          1 - j*(1.0/this.stacks));
+            this.texCoords.push(0+i*(1.0/this.slices),                            1 - ((1.0/this.stacks) + j*(1.0/this.stacks)));
+            this.texCoords.push((1.0/this.slices) + i*(1.0/this.slices),          1 - ((1.0/this.stacks) + j*(1.0/this.stacks)));
+
+            x1_bottom = x1_top;
+            x2_bottom = x2_top;
+            y1_bottom = y1_top;
+            y2_bottom = y2_top;
+          }
+          ang+=alphaAng;
         }
-        ang+=alphaAng;
-      }
 
-      this.primitiveType = this.scene.gl.TRIANGLES;
-      this.initGLBuffers();
-  }
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
 }
 
---
+++ T1_test_noparser/tp1/MyTorus.js
@@ -0,0 +1,68 @@
+/**
+* MyTorus
+* @constructor
+*/
+
+class MyTorus extends CGFobject {
+    //loops = loops;
+    constructor(scene, slices, innerRadius, outerRadius, loops) {
+        super(scene);
+        this.scene = scene;
+
+        this.slices = slices;
+        this.innerRadius = innerRadius;
+        this.outerRadius = outerRadius;
+        this.loops = loops;
+
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        let delta_phi = 2*Math.PI/this.slices;
+        let delta_theta = 2*Math.PI/this.loops;
+
+        for(let i = 0; i <= this.slices; i++) {
+			for(let j = 0; j <= this.loops; j++) {
+                
+                let x = (this.outerRadius + this.innerRadius*Math.cos(delta_theta*j))*Math.cos(delta_phi*i);
+                let y = (this.outerRadius + this.innerRadius*Math.cos(delta_theta*j))*Math.sin(delta_phi*i);
+                
+                let nx = Math.cos(delta_theta*j)*Math.cos(delta_phi*i);
+                let ny = Math.cos(delta_theta*j)*Math.sin(delta_phi*i);
+                let nz = Math.sin(delta_theta*j);
+
+                this.vertices.push(x, y, this.innerRadius*nz);
+                this.normals.push(nx, ny, this.innerRadius*nz);
+                this.texCoords.push(1-1/this.slices, 1-1/this.loops);
+            }
+        }
+
+        for(let i = 0; i < this.slices; i++) {
+			for(let j = 0; j < this.loops; j++) {
+                
+                let a = (i+1)*(this.loops+1) + j;
+				let b = i*(this.loops+1) + j+1;
+				let c = i*(this.loops+1) + j;
+
+				let d = i*(this.loops+1) + j+1;
+				let e = (i+1)*(this.loops+1) + j;
+				let f = (i+1)*(this.loops+1) + j+1;
+
+				this.indices.push(a, b, c);
+				this.indices.push(d, e, f);
+				//Both sides visible
+				this.indices.push(c, b, a);
+				this.indices.push(f, e, d);
+			}
+		}
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+}
+
