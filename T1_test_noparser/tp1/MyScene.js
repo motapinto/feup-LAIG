d74456c0d24d681e0fc50807a200c204a6c72f11
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-10-04 08:25:13 +0100

    scene no parser

--- T1_test_noparser/tp1/MyScene.js
+++ T1_test_noparser/tp1/MyScene.js
@@ -34,8 +34,8 @@ class MyScene extends CGFscene {
         this.restaurantTerrain = new MyRectangle(this, -1, 12, -1, 20);
         this.terrain = new MyRectangle(this, -20, 20, -20, 20);
         //BALCAO
-        this.balcaoCenter = new MyCylinder(this, 1, 1, 1.5, 4, 4);
-        this.balcaoTop = new MyCylinderTops(this, 1, 1, 1.5, 4, 4, 5);
+        this.balcaoCenter = new MyCylinder(this, 0.7, 0.7, 1.5, 4, 4);
+        this.balcaoTop = new MyCylinderTops(this, 1, 1, 0.3, 4, 4, 5);
         this.balcaoChairLeg = new MyCylinder(this, 0.1, 0.1, 1.2, 20, 5);
         this.balcaoChairBase = new MyCylinderTops(this, 0.45, 0.45, 0.07, 4, 4, 4);
         this.balcaoChairSupport = new MyCylinderTops(this, 0.45, 0.45, 0.07, 4, 4, 4);
@@ -43,6 +43,8 @@ class MyScene extends CGFscene {
         this.door = new MyCylinderTops(this, 0.75, 0.75, 2.5, 4, 4, 4);
         this.doorHandleSupport = new MyCylinder(this, 0.03, 0.03, 0.3, 4, 5);
         this.doorHandle = new MyCylinder(this, 0.03, 0.03, 0.4, 4, 5);
+        //WALLS
+        this.wall = new MyCylinderTops(this, 6, 6, 3.5, 4, 4, 4);
 
         //DONOUT LOGO
         this.logo = new MyTorus(this, 40, 0.5, 1, 40);
@@ -114,298 +116,84 @@ class MyScene extends CGFscene {
     }
 
     displayChairs() {
+        for(let i=0; i<3; i++) {
+            for(let j=0; j<4; j++) {
+                this.pushMatrix();
+                    //CHAIR1
+                    this.translate(3*j, 3*i, 0.5);
+                    //LEGS
+                    this.pushMatrix();
+                        this.translate(0.3, 0, -0.5);
+                        this.chairLeg.display();
+                    this.popMatrix();
+                    this.pushMatrix();
+                        this.translate(0, 0.3, -0.5);
+                        this.chairLeg.display();
+                    this.popMatrix();
+                    this.pushMatrix();
+                        this.translate(-0.40, -0.05, -0.5);
+                        this.chairLeg2.display();
+                    this.popMatrix();
+                    this.pushMatrix();
+                        this.translate(-0.05, -0.40, -0.5);
+                        this.chairLeg2.display();
+                    this.popMatrix();
+                    
+                    //BASE
+                    this.pushMatrix();
+                        this.chairBase.display();
+                    this.popMatrix();
         
-        //CHAIR
-        this.pushMatrix();
-            this.translate(0, 0, 0.5);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
+                    //SUPPORT
+                    this.pushMatrix();
+                        this.translate(-0.248, -0.248, 0.66)
+                        this.rotate(Math.PI/4, 0, 0, 1)
+                        this.rotate(Math.PI/4, 1, 0, 0)
+                        this.rotate(Math.PI/2, 0, 1, 0)
+                        this.chairSupport.display();
+                    this.popMatrix();
 
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
+                this.popMatrix();
+                
+                //CHAIR2
+                this.pushMatrix();
+                    this.translate(1+3*j, 1+3*i, 0.5);
+                    this.rotate(Math.PI, 0, 0, 1);
+                    //LEGS
+                    this.pushMatrix();
+                        this.translate(0.3, 0, -0.5);
+                        this.chairLeg.display();
+                    this.popMatrix();
+                    this.pushMatrix();
+                        this.translate(0, 0.3, -0.5);
+                        this.chairLeg.display();
+                    this.popMatrix();
+                    this.pushMatrix();
+                        this.translate(-0.40, -0.05, -0.5);
+                        this.chairLeg2.display();
+                    this.popMatrix();
+                    this.pushMatrix();
+                        this.translate(-0.05, -0.40, -0.5);
+                        this.chairLeg2.display();
+                    this.popMatrix();
+                    
+                    //BASE
+                    this.pushMatrix();
+                        this.chairBase.display();
+                    this.popMatrix();
 
-        //CHAIR
-        this.pushMatrix();
-            this.translate(0, 3, 0.5);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
-
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
-
-        //CHAIR
-        this.pushMatrix();
-            this.translate(3, 3, 0.5);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
-
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
-
-        //CHAIR
-        this.pushMatrix();
-            this.translate(3, 0, 0.5);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
-
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
-
-        //CHAIR
-        this.pushMatrix();
-            this.translate(1, 1, 0.5);
-            this.rotate(Math.PI, 0, 0, 1);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
-
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
-
-        //CHAIR
-        this.pushMatrix();
-            this.translate(1, 4, 0.5);
-            this.rotate(Math.PI, 0, 0, 1);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
-
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
-
-        //CHAIR
-        this.pushMatrix();
-            this.translate(4, 1, 0.5);
-            this.rotate(Math.PI, 0, 0, 1);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
-
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
-
-        //CHAIR
-        this.pushMatrix();
-            this.translate(4, 4, 0.5);
-            this.rotate(Math.PI, 0, 0, 1);
-            //LEGS
-            this.pushMatrix();
-                this.translate(0.3, 0, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(0, 0.3, -0.5);
-                this.chairLeg.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.40, -0.05, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            this.pushMatrix();
-                this.translate(-0.05, -0.40, -0.5);
-                this.chairLeg2.display();
-            this.popMatrix();
-            
-            //BASE
-            this.pushMatrix();
-                this.chairBase.display();
-            this.popMatrix();
-
-            //SUPPORT
-            this.pushMatrix();
-                this.translate(-0.248, -0.248, 0.66)
-                this.rotate(Math.PI/4, 0, 0, 1)
-                this.rotate(Math.PI/4, 1, 0, 0)
-                this.rotate(Math.PI/2, 0, 1, 0)
-                this.chairSupport.display();
-            this.popMatrix();
-        this.popMatrix();
+                    //SUPPORT
+                    this.pushMatrix();
+                        this.translate(-0.248, -0.248, 0.66)
+                        this.rotate(Math.PI/4, 0, 0, 1)
+                        this.rotate(Math.PI/4, 1, 0, 0)
+                        this.rotate(Math.PI/2, 0, 1, 0)
+                        this.chairSupport.display();
+                    this.popMatrix();
+                
+                this.popMatrix();
+            }
+        }
     }
 
     displayTables() {
@@ -443,7 +231,7 @@ class MyScene extends CGFscene {
 
     displayDoor() {
         this.pushMatrix();
-            this.translate(12, 10, 0)
+            this.translate(12, 9.5, 0)
             this.pushMatrix();
                 this.scale(0.1, 1, 1)
                 this.rotate(Math.PI/4, 0, 0, 1)
@@ -455,6 +243,12 @@ class MyScene extends CGFscene {
                 this.rotate(-Math.PI/2, 0, 1, 0)
                 this.doorHandle.display();
                 this.translate(0, 0.01, 0)
+                this.rotate(Math.PI/2, 1, 0, 0)
+                this.doorHandleSupport.display()
+            this.popMatrix();
+
+            this.pushMatrix();
+                this.translate(-0.2, 0.2, 1)
                 this.rotate(Math.PI/2, 1, 0, 0)
                 this.doorHandleSupport.display()
             this.popMatrix();
@@ -477,30 +271,32 @@ class MyScene extends CGFscene {
     }
 
     displayBalcao() {
-        for(let i=0;i<5;i++) {
+        for(let i=0;i<8;i++) {
             this.pushMatrix();
-                this.translate(2+i*1.41, 18, 0);
+                this.translate(2+i*0.98, 17.8, 1.5);
+                this.scale(0.7, 1, 1);
                 this.rotate(Math.PI/4, 0, 0, 1);
                 
                 this.balconyTop.apply();
                 this.balcaoTop.display();
             this.popMatrix();
-
+            
             this.pushMatrix();
-                this.translate(2+i*1.41, 18, 0);
+                this.translate(2+i*0.98, 18, 0);
                 this.rotate(Math.PI/4, 0, 0, 1);
                 
                 this.balconyCenter.apply();
                 this.balcaoCenter.display(); 
 
             this.popMatrix();
+
         }
     }
 
     displayBalcaoChair() {
-        for(let j=0; j<5;j++) {//y
+        for(let j=0; j<8;j++) {//y
                 this.pushMatrix();
-                this.translate(1.41*j+2, 16, 0);
+                this.translate(0.98*j+2, 16, 0);
                 this.rotate(Math.PI/4, 0,0,1);
                     
                     this.pushMatrix();
@@ -520,6 +316,24 @@ class MyScene extends CGFscene {
 
                 this.popMatrix();
         }
+    }
+
+    displayWalls() {
+        this.pushMatrix();
+            this.pushMatrix();
+                this.translate(12, 3.5, 0);
+                this.scale(0.01, 1, 1);
+                this.rotate(Math.PI/4, 0,0,1);
+                this.wall.display();
+            this.popMatrix();
+
+            this.pushMatrix();
+                this.translate(12, 15.5, 0);
+                this.scale(0.01, 1, 1);
+                this.rotate(Math.PI/4, 0,0,1);
+                this.wall.display();
+            this.popMatrix();
+        this.popMatrix();
     }
 
     display() {
@@ -552,6 +366,7 @@ class MyScene extends CGFscene {
         this.displayBalcao();
         this.displayBalcaoChair();
         this.displayLogo();
+        this.displayWalls();
         this.displayDoor();
         //this.material.apply();
         
