3bccdac2739d9495203d7c96d2c3ea74d53709ea
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-10-10 23:17:25 +0100

    before t3 commit

--- T1/reader/MyInterface.js
+++ T1/reader/MyInterface.js
@@ -27,6 +27,7 @@ class MyInterface extends CGFinterface {
 
         this.gui.add(this.scene, 'scaleFactor', 0.1, 10.0).name('Scale');
 		this.gui.add(this.scene, 'displayAxis').name("Display axis"); //checkbox
+		this.gui.add(this.scene, 'floor').name("Floor Number").min(0).step(1); //number
 
         return true;
     }
