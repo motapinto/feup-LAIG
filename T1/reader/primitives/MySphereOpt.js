48b5bc045c78182a568fccde1feb539fdd3995e6
LuisPRamos <49035388+LuisPRamos@users.noreply.github.com>
2019-10-15 20:10:20 +0100

    Sphere optimized

--- T1/reader/primitives/MySphereOpt.js
+++ T1/reader/primitives/MySphereOpt.js
@@ -24,11 +24,13 @@ class MySphere extends CGFobject
 		var teta_delta = Math.PI/(2*this.stacks);
 		var phi_delta = 2*Math.PI/this.slices;
 
+		var j_max = (((this.slices >> 1) + 1) >> 1) + 1; //round this.slices/4 and add 1
+
 		//Vertices and normals generation
-		for(var i = 0; i < this.stacks; i++) {
+		for(var i = 0; i < (this.stacks + 1); i++) {
 	
 			// 1 more cycle to avoid special cases in the connect parts of the quadrants
-			for(var j = 0; j < ((this.slices / 4) + 1); j++) {
+			for(var j = 0; j < j_max ; j++) {
                 
                 var x = Math.cos(teta_delta*i)*Math.cos(phi_delta*j);
                 var y = Math.cos(teta_delta*i)*Math.sin(phi_delta*j);
@@ -56,22 +58,20 @@ class MySphere extends CGFobject
 				this.vertices.push(-x, y, -z);	//6th Quadrant
 				this.vertices.push(-x, -y, -z);	//7th Quadrant
 				this.vertices.push(x, -y, -z);	//8th Quadrant
-                
+
+				
+				//Tex coords
+				
 			}
 		}
-		this.normals.push(0, 0, 1); //Top
-		this.normals.push(0, 0, -1); //Bottom
-
-		this.vertices.push(0, 0, radius); //Top
-		this.vertices.push(0, 0, -radius); //Bottom
 
 
 		//Indices generation
 		var quadrant_delta = 8;
-		var vertices_stack = ((this.slices / 4) + 1) * 8; //Number of vertices per stack
+		var vertices_stack = j_max * 8; //Number of vertices per stack
 
 		for(var i = 0; i < this.stacks; i++){
-			for(var j = 0; j < (this.slices / 4); j++){
+			for(var j = 0; j < (j_max - 1); j++){
 				//Check for top of sphere
 				if(i != (this.stacks - 1)){
 
@@ -89,103 +89,103 @@ class MySphere extends CGFobject
 						j * quadrant_delta + (i + 1) * vertices_stack
 					);
 				
-					// // 2nd Quadrant
-					// //Bottom left triangle
-					// this.indices.push(	
-					// 	1 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	1 + j * quadrant_delta + i * vertices_stack,
-					// 	1 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-					// //Top right triangle
-					// this.indices.push(	
-					// 	1 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	1 + j * quadrant_delta + (i + 1) * vertices_stack,
-					// 	1 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
-					// );
+					// 2nd Quadrant
+					//Bottom left triangle
+					this.indices.push(	
+						1 + (j + 1) * quadrant_delta + i * vertices_stack,
+						1 + j * quadrant_delta + i * vertices_stack,
+						1 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+					//Top right triangle
+					this.indices.push(	
+						1 + (j + 1) * quadrant_delta + i * vertices_stack,
+						1 + j * quadrant_delta + (i + 1) * vertices_stack,
+						1 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 3rd Quadrant
-					// //Bottom left triangle
-					// this.indices.push(	
-					// 	2 + j * quadrant_delta + i * vertices_stack,
-					// 	2 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	2 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-					// //Top right triangle
-					// this.indices.push(	
-					// 	2 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	2 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack,
-					// 	2 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-				
-					// // 4th Quadrant
-					// //Bottom left triangle
-					// this.indices.push(	
-					// 	3 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	3 + j * quadrant_delta + i * vertices_stack,
-					// 	3 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-					// //Top right triangle
-					// this.indices.push(	
-					// 	3 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	3 + j * quadrant_delta + (i + 1) * vertices_stack,
-					// 	3 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
-					// );
+					// 3rd Quadrant
+					//Bottom left triangle
+					this.indices.push(	
+						2 + j * quadrant_delta + i * vertices_stack,
+						2 + (j + 1) * quadrant_delta + i * vertices_stack,
+						2 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+					//Top right triangle
+					this.indices.push(	
+						2 + (j + 1) * quadrant_delta + i * vertices_stack,
+						2 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack,
+						2 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+			
+					// 4th Quadrant
+					//Bottom left triangle
+					this.indices.push(	
+						3 + (j + 1) * quadrant_delta + i * vertices_stack,
+						3 + j * quadrant_delta + i * vertices_stack,
+						3 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+					//Top right triangle
+					this.indices.push(	
+						3 + (j + 1) * quadrant_delta + i * vertices_stack,
+						3 + j * quadrant_delta + (i + 1) * vertices_stack,
+						3 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 5th Quadrant
-					// //Bottom left triangle
-					// this.indices.push(	
-					// 	4 + j * quadrant_delta + i * vertices_stack,
-					// 	4 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	4 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-					// //Top right triangle
-					// this.indices.push(	
-					// 	4 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	4 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack,
-					// 	4 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
+					// 5th Quadrant
+					//Bottom left triangle
+					this.indices.push(	
+						4 + (j + 1) * quadrant_delta + i * vertices_stack,
+						4 + j * quadrant_delta + i * vertices_stack,
+						4 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+					//Top right triangle
+					this.indices.push(	
+						4 + (j + 1) * quadrant_delta + i * vertices_stack,
+						4 + j * quadrant_delta + (i + 1) * vertices_stack,
+						4 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 6th Quadrant
-					// //Bottom left triangle
-					// this.indices.push(	
-					// 	5 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	5 + j * quadrant_delta + i * vertices_stack,
-					// 	5 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-					// //Top right triangle
-					// this.indices.push(	
-					// 	5 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	5 + j * quadrant_delta + (i + 1) * vertices_stack,
-					// 	5 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
-					// );
+					// 6th Quadrant
+					//Bottom left triangle
+					this.indices.push(	
+						5 + j * quadrant_delta + i * vertices_stack,
+						5 + (j + 1) * quadrant_delta + i * vertices_stack,
+						5 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+					//Top right triangle
+					this.indices.push(	
+						5 + (j + 1) * quadrant_delta + i * vertices_stack,
+						5 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack,
+						5 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 7th Quadrant
-					// //Bottom left triangle
-					// this.indices.push(	
-					// 	6 + j * quadrant_delta + i * vertices_stack,
-					// 	6 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	6 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-					// //Top right triangle
-					// this.indices.push(	
-					// 	6 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	6 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack,
-					// 	6 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
+					// 7th Quadrant
+					//Bottom left triangle
+					this.indices.push(	
+						6 + (j + 1) * quadrant_delta + i * vertices_stack,
+						6 + j * quadrant_delta + i * vertices_stack,
+						6 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+					//Top right triangle
+					this.indices.push(	
+						6 + (j + 1) * quadrant_delta + i * vertices_stack,
+						6 + j * quadrant_delta + (i + 1) * vertices_stack,
+						6 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 8th Quadrant
-					// //Bottom left triangle
-					// this.indices.push(	
-					// 	7 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	7 + j * quadrant_delta + i * vertices_stack,
-					// 	7 + j * quadrant_delta + (i + 1) * vertices_stack
-					// );
-					// //Top right triangle
-					// this.indices.push(	
-					// 	7 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	7 + j * quadrant_delta + (i + 1) * vertices_stack,
-					// 	7 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack
-					// );
+					// 8th Quadrant
+					//Bottom left triangle
+					this.indices.push(	
+						7 + j * quadrant_delta + i * vertices_stack,
+						7 + (j + 1) * quadrant_delta + i * vertices_stack,
+						7 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
+					//Top right triangle
+					this.indices.push(	
+						7 + (j + 1) * quadrant_delta + i * vertices_stack,
+						7 + (j + 1) * quadrant_delta + (i + 1) * vertices_stack,
+						7 + j * quadrant_delta + (i + 1) * vertices_stack,
+					);
 				}
 				//Top of sphere
 				else{
@@ -194,57 +194,57 @@ class MySphere extends CGFobject
 					this.indices.push(	
 						j * quadrant_delta + i * vertices_stack,
 						(j + 1) * quadrant_delta + i * vertices_stack,
-						this.vertices.length/3 - 2
+						j * quadrant_delta + (i + 1) * vertices_stack
 					);
 				
-					// // 2nd Quadrant
-					// this.indices.push(	
-					// 	1 + j * quadrant_delta + i * vertices_stack,
-					// 	1 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	this.vertices.length/3 - 2
-					// );
+					// 2nd Quadrant
+					this.indices.push(	
+						1 + (j + 1) * quadrant_delta + i * vertices_stack,
+						1 + j * quadrant_delta + i * vertices_stack,
+						2 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 3rd Quadrant
-					// this.indices.push(	
-					// 	2 + j * quadrant_delta + i * vertices_stack,
-					// 	2 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	this.vertices.length/3 - 2
-					// );
+					// 3rd Quadrant
+					this.indices.push(	
+						2 + j * quadrant_delta + i * vertices_stack,
+						2 + (j + 1) * quadrant_delta + i * vertices_stack,
+						2 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 				
-					// // 4th Quadrant
-					// this.indices.push(	
-					// 	3 + j * quadrant_delta + i * vertices_stack,
-					// 	3 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	this.vertices.length/3 - 2
-					// );
+					// 4th Quadrant
+					this.indices.push(	
+						3 + (j + 1) * quadrant_delta + i * vertices_stack,
+						3 + j * quadrant_delta + i * vertices_stack,
+						3 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 5th Quadrant
-					// this.indices.push(	
-					// 	4 + j * quadrant_delta + i * vertices_stack,
-					// 	4 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	this.vertices.length/3 - 1
-					// );
+					// 5th Quadrant
+					this.indices.push(	
+						4 + (j + 1) * quadrant_delta + i * vertices_stack,
+						4 + j * quadrant_delta + i * vertices_stack,
+						4 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 6th Quadrant
-					// this.indices.push(	
-					// 	5 + j * quadrant_delta + i * vertices_stack,
-					// 	5 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	this.vertices.length/3 - 1
-					// );
+					// 6th Quadrant
+					this.indices.push(	
+						5 + j * quadrant_delta + i * vertices_stack,
+						5 + (j + 1) * quadrant_delta + i * vertices_stack,
+						5 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 7th Quadrant
-					// this.indices.push(	
-					// 	6 + j * quadrant_delta + i * vertices_stack,
-					// 	6 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	this.vertices.length/3 - 1
-					// );
+					// 7th Quadrant
+					this.indices.push(	
+						6 + (j + 1) * quadrant_delta + i * vertices_stack,
+						6 + j * quadrant_delta + i * vertices_stack,
+						6 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 
-					// // 8th Quadrant
-					// this.indices.push(	
-					// 	7 + j * quadrant_delta + i * vertices_stack,
-					// 	7 + (j + 1) * quadrant_delta + i * vertices_stack,
-					// 	this.vertices.length/3 - 1
-					// );
+					// 8th Quadrant
+					this.indices.push(	
+						7 + j * quadrant_delta + i * vertices_stack,
+						7 + (j + 1) * quadrant_delta + i * vertices_stack,
+						7 + j * quadrant_delta + (i + 1) * vertices_stack
+					);
 				}
 			}
 		}
