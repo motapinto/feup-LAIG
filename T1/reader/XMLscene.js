21cf10cf8cef970355ea73673cd84c7962d11a43
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2019-10-18 09:59:03 +0100

    UPDATE

--- T1/reader/XMLscene.js
+++ T1/reader/XMLscene.js
@@ -35,7 +35,7 @@ class XMLscene extends CGFscene {
 
         this.axis = new CGFaxis(this);
         this.displayAxis = false;
-        this.scaleFactor = 0.1;
+        this.scaleFactor = 1;
         this.floor = 0;
         this.floorMax = 0;
         this.viewsList = [];
