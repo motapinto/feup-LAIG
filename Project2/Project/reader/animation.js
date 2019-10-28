97a52ffb68f6bc8345b55b77321a0501e1ca7809
luis ramos <deco.luis@live.com.pt>
2019-10-28 09:39:32 +0000

    keyframeAnimation added

--- Project2/Project/reader/animation.js
+++ Project2/Project/reader/animation.js
@@ -1,8 +1,38 @@
-
+/**
+ * Animation class, representing the base class for Keyframe Animations.
+ */
 class Animation {
+    /**
+   * @constructor
+   * @param {scene} scene
+   * @param {integer} deltaTime
+   * @param {vec3} translateInit
+   * @param {vec3} translateInit
+   * @param {vec3} rotateInit
+   * @param {vec3} rotateFinal
+   * @param {vec3} scaleInit
+   * @param {vec3} scaleFinal
+   * @param {integer} updatePeriod
+   */
   constructor (scene, deltaTime, translateInit, translateFinal, rotateInit, rotateFinal, scaleInit, scaleFinal, updatePeriod) {
+    if (this.constructor === Animation) {
+      throw new TypeError('Abstract class "Animation" cannot be instantiated directly.'); 
+    }
+
+    if (this.update === undefined) {
+      throw new TypeError('Classes extending the Animation abstract class');
+    }
+  
+    if (this.apply === undefined) {
+      throw new TypeError('Classes extending the Animation abstract class');
+    }
+
+  this.scene = scene;
+
     this.deltaTime = deltaTime;
+
     this.framesPerKeyFrame = deltaTime*1000 / updatePeriod;
+
     this.linearVelocity = [
       (translateFinal[0] - translateInit[0]) / deltaInstant,
       (translateFinal[1] - translateInit[1]) / deltaInstant,
@@ -23,35 +53,4 @@ class Animation {
 
     this.matrix = mat4.create();
   }
-
-  update(instant){
-    //maximum instant is equal to deltaTime
-    if(instant > this.deltaTime)
-      instant = this.deltaTime;
-
-    //Number of curent frame in keyframe
-    var nFrame = (this.framesPerKeyFrame * instant) / this.deltaTime;
-
-    //create new matrix
-    this.matrix = mat4.create();
-
-    //translate
-    mat4.translate(this.matrix, this.matrix, vec3.fromValues(instant*this.linearVelocity[0], instant*this.linearVelocity[1], instant*this.linearVelocity[2]));
-  
-    //rotate on X
-    mat4.rotate(this.matrix, this.matrix, (this.angularVelocity[0] * instant) * Math.PI / 180, 1, 0, 0);
-    //rotate on Y
-    mat4.rotate(this.matrix, this.matrix, (this.angularVelocity[1] * instant) * Math.PI / 180, 0, 1, 0);
-    //rotate on Z
-    mat4.rotate(this.matrix, this.matrix, (this.angularVelocity[2] * instant) * Math.PI / 180, 0, 0, 1);
-
-    //scale
-    mat4.scale(this.matrix, this.matrix, vec3.fromValues(Math.pow(this.scaleR[0], nFrame), Math.pow(this.scaleR[1], nFrame), Math.pow(this.scaleR[2], nFrame)));
-  }
-
-  apply(){
-    this.scene.multMatrix(this.matrix);
-  }
-
-
 }
