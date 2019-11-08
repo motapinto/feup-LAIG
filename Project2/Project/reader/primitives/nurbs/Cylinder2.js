3d4b053baff6bf7808df6f58cc6825e568e88a3e
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-11-08 00:05:58 +0000

    before tp commit

--- Project2/Project/reader/primitives/nurbs/Cylinder2.js
+++ Project2/Project/reader/primitives/nurbs/Cylinder2.js
@@ -11,6 +11,24 @@ class Cylinder2 extends CGFobject{
   constructor (scene, base, top, height, slices, stacks) {
         super(scene);
         this.scene = scene;
+        let controlPoints = [[]];
+        let posCounter = 0;
+
+        //Control Points Benzier
+        /*for(let uDiv = 0; uDiv < slices; uDiv++) {
+          controlPoints[uDiv] = [];
+          let xBase = base*Math.cos((slices*Math.PI) / slices);
+          let zBase = base*Math.sin((slices*Math.PI) / slices);
+          let xTop = top*Math.cos((slices*Math.PI) / slices);
+          let zTop = top*Math.sin((slices*Math.PI) / slices);
+          for(let vDiv = 0; vDiv < stacks; vDiv++) {
+            controlPoints[uDiv][vDiv] = [xBase, 0, zBase, 1];
+            controlPoints[uDiv][vDiv] = [xTop, vDiv*(stacks/height), zTop, 1];
+            posCounter++;
+          }
+        }*/
+
+        //let xBase = base*Math.cos(((360/slices)*Math.PI) / 360);
 
         //Example of control points for patch
         let controlPoints1 = 
@@ -33,7 +51,7 @@ class Cylinder2 extends CGFobject{
         ];
 
         let nurbsSurface = new CGFnurbsSurface(2, 1, controlPoints1);
-        this.obj = new CGFnurbsObject(scene, 5, 5, nurbsSurface);
+        this.obj = new CGFnurbsObject(scene, 20, 2, nurbsSurface);
   }
 
   display() {
