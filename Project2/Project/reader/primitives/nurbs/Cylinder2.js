f5be3603131069bc254e638a551b64ccf5ce0e1d
luis ramos <deco.luis@live.com.pt>
2019-11-22 11:25:47 +0000

    mid class

--- Project2/Project/reader/primitives/nurbs/Cylinder2.js
+++ Project2/Project/reader/primitives/nurbs/Cylinder2.js
@@ -17,64 +17,38 @@ class Cylinder2 extends CGFobject{
         
         stacks = stacks + 1;
 
-        let controlPoints1 = [[]];
-        let controlPoints2 = [[]];
+        let controlPoints = [[]];
 
         //Control Points Benzier - P1
-        controlPoints1[0] = [];
+        controlPoints[0] = [];
         for(let vDiv = 0; vDiv < stacks; vDiv++) {
           let radius = base + radiusIncrement*vDiv;
-          controlPoints1[0][vDiv] = [-radius, 0, vDiv*heightIncrement, 1];
+          controlPoints[0][vDiv] = [-radius, 0, vDiv*heightIncrement, 1];
         }
 
         //Control Points Benzier - P2
-        controlPoints1[1] = [];
+        controlPoints[1] = [];
         for(let vDiv = 0; vDiv < stacks; vDiv++) {
           let radius = base + radiusIncrement*vDiv;
-          controlPoints1[1][vDiv] = [-radius, -(4*radius)/3, vDiv*heightIncrement, 1];
+          controlPoints[1][vDiv] = [-radius, -(4*radius)/3, vDiv*heightIncrement, 1];
         }
 
         //Control Points Benzier - P3
-        controlPoints1[2] = [];
+        controlPoints[2] = [];
         for(let vDiv = 0; vDiv < stacks; vDiv++) {
           let radius = base + radiusIncrement*vDiv;
-          controlPoints1[2][vDiv] = [radius, -(4*radius)/3, vDiv*heightIncrement, 1];
+          controlPoints[2][vDiv] = [radius, -(4*radius)/3, vDiv*heightIncrement, 1];
         }
 
         //Control Points Benzier - P4
-        controlPoints1[3] = [];
+        controlPoints[3] = [];
         for(let vDiv = 0; vDiv < stacks; vDiv++) {
           let radius = base + radiusIncrement*vDiv;
-          controlPoints1[3][vDiv] = [radius, 0, vDiv*heightIncrement, 1];
+          controlPoints[3][vDiv] = [radius, 0, vDiv*heightIncrement, 1];
         }
 
-        //let xBase = base*Math.cos(((360/slices)*Math.PI) / 360);
 
-        //Example of control points for semi cylinder
-        /*let controlPoints69 = 
-        [	// U = 0
-          [ // V = 0..1;
-            [ 0, 0, 0, 1 ],
-            [ 0,  1, 0, 1 ]
-          ],
-          // U = 1
-          [ // V = 0..1
-            [ 0, 0, 2, 1 ],
-            [ 0,  1, 2, 1 ]							 
-          ],
-          // U = 2
-          [ // V = 0..1							 
-            [ 3, 0, 2, 1 ],
-            [ 3,  1, 2, 1 ]
-          ],
-           // U = 3
-           [ // V = 0..1							 
-            [ 3, 0, 0, 1 ],
-            [ 3,  1, 0, 1 ]
-          ]
-        ];*/
-
-        let nurbsSurface = new CGFnurbsSurface(3, stacks-1, controlPoints1);
+        let nurbsSurface = new CGFnurbsSurface(3, stacks-1, controlPoints);
         this.obj = new CGFnurbsObject(scene, slices, stacks, nurbsSurface);
   }
 
