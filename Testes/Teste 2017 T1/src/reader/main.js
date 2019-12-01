2a7df2851c0eb62538d4141435f303fe0f36bb4f
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-12-01 17:54:27 +0000

    prob3

--- Testes/Teste 2017 T1/src/reader/main.js
+++ Testes/Teste 2017 T1/src/reader/main.js
@@ -52,7 +52,7 @@ main=function()
 
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
-    let filename=getUrllets()['file'] || "prob2.xml";
+    let filename=getUrllets()['file'] || "prob3.xml";
 
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
