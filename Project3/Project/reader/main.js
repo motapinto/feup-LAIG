dd3deb18e2a8923e0d913efb7c7c6704930402c9
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-12-14 20:11:19 +0000

    nao mostra a table

--- Project3/Project/reader/main.js
+++ Project3/Project/reader/main.js
@@ -59,7 +59,7 @@ main=function()
 
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
-    let filename=getUrllets()['file'] || "board.xml";
+    let filename=getUrllets()['file'] || "maini.xml";
 
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
